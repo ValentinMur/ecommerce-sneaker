75a485a91ba43dabe188e31fe72da0b9b3a73329
Valentin Mur <valen.mur88@gmail.com>
2022-07-11 16:31:33 -0300

    Simulador interactivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,28 +51,146 @@ window.onscroll = () => {
 }
 
 
-/*===== DESAFIOS CODER =====*/
+///////////*===== DESAFIO COMPLEMENTARIO CODER =====*////////////////
 
-for (i = 1; i <= 10; i++) {
-    console.log(i)
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// let precio = parseFloat(prompt('INGRESAR NUMERO'));
+// let descuento20 = precio - (precio * 0.2);
+// console.log(descuento20);
+
+
+// const sneakers1 = prompt('FIRST SNEAKER');
+// const sneakers2 = prompt('SECOND SNEAKER');
+// const sneakers3 = prompt('THIRD SNEAKER');
+// const sneakers4 = prompt('QUARTER SNEAKER');
+// const sneakers5 = prompt('FIFTH SNEAKER');
+// const espacio = ' ';
+
+// if ((sneakers1 != '') && (sneakers2 != '') && (sneakers3 != '') && (sneakers4 != '') && (sneakers5 != '')) {
+//     let carrito = sneakers1 + ' ' + sneakers2 + ' ' + sneakers3 + espacio + sneakers4 + espacio + sneakers5
+//     console.log(carrito);
+// } else {
+
+//     console.log('ERROR NEED FIVE SNEAKERS')
+// }
+
+// function Perro(nombre, edad, vivo) {
+//     this.nombre = nombre;
+//     this.edad = edad;
+//     this.vivo = vivo;
+// };
+
+// const labrador = new Perro('inca', 20, false);
+// console.log(labrador);
+
+// const caniche = new Perro('tobi', 7, true);
+// console.log(caniche);
+
+// function Human(name, country, age) {
+//     this.name = name;
+//     this.country = country;
+//     this.age = age;
+// };
+
+// const asian = new Human('VinoToro', 'Argentinian', 23);
+// console.log(asian);
+
+
+// class Celular {
+//     constructor(color, peso, rdp, rdc, ram) {
+//         this.color = color;
+//         this.peso = peso;
+//         this.resolucionDePantalla = rdp;
+//         this.resolucionDeCamara = rdc;
+//         this.memoriaRam = ram;
+//         this.encendido = false;
+//     }
+//     botonEncendido() {
+//         if (this.encendido == false) {
+//             alert('celular prendido')
+//             this.encendido = true;
+//         } else {
+//             alert('celular apagado');
+//         }
+//     }
+
+//     reiniciar() {
+//         if (this.encendido == true) {
+//             alert('reiniciando celular');
+//         } else {
+//             alert('el celular esta apagado')
+//         }
+//     }
+
+//     tomarFoto() {
+//         alert(`foto tomada en una resolucion de: ${this.resolucionDeCamara}`)
+//     }
+
+//     grabarVideo() {
+//         alert(`grabar video en: ${this.resolucionDeCamara}`)
+//     }
+// }
+
+
+// celular1 = new Celular('rojo', '150g', '5', 'full hd', '2GB');
+
+
+
+
+////////////////////////*===== DESAFIO ENTREGABLE CODER =====*////////////////////
+
+
+
+
+// const arrayProductos = ['Nike Free RN, ' + 'Adidas Flex, ' + 'Nike Jordan']
+
+
+
+// for (let i = 0; i < arrayProductos.length; i++) {
+//     console.log(arrayProductos[i])
+// }
+
+class Producto {
+    constructor(nombre, precio, stock) {
+        this.nombre = nombre,
+            this.precio = Number(precio),
+            this.stock = stock
+    };
 }
 
-let precio = parseFloat(prompt('INGRESAR NUMERO'));
-let descuento20 = precio - (precio * 0.2);
-console.log(descuento20);
+let nikeFreeRn = new Producto('Nike Free Rn', 100, 10)
+let adidasFlex = new Producto('Adidas Flex', 70, 10)
+let nikeJordan = new Producto('Nike Jordan', 100, 10)
+
+// alert('Hola bienvenidos a ruby\n\nestos son los productos en el catalogo \n\n *Nike Free RN $100\n *Adidas Flex $70\n *Nike Jordan $100');
 
 
-let sneakers1 = prompt('FIRST SNEAKER');
-let sneakers2 = prompt('SECOND SNEAKER');
-let sneakers3 = prompt('THIRD SNEAKER');
-let sneakers4 = prompt('QUARTER SNEAKER');
-let sneakers5 = prompt('FIFTH SNEAKER');
-let espacio = ' ';
 
-if ((sneakers1 != '') && (sneakers2 != '') && (sneakers3 != '') && (sneakers4 != '') && (sneakers5 != '')) {
-    let carrito = sneakers1 + ' ' + sneakers2 + ' ' + sneakers3 + espacio + sneakers4 + espacio + sneakers5
-    console.log(carrito);
-} else {
+function agregarCarrito(producto, stock) {
+    const tenemosStock = validarStock(stock);
+    if (tenemosStock === 'tenemos stock') {
+        console.log('Agregas producto al carrito: ' + producto + ' ' + stock);
+    } else {
+        console.log('No hay productos agregados');
+    }
 
-    console.log('ERROR NEED FIVE SNEAKERS')
+
 }
+
+
+function validarStock(stockdelProducto) {
+    if (stockdelProducto > 0) {
+        return 'tenemos stock'
+    } else {
+        return 'no hay stock'
+    }
+}
+
+function precioFinal() { }
+
+
+agregarCarrito('Nike Jordan', 1)
+agregarCarrito('Nike Air', 5)
