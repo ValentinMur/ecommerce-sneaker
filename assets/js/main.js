648edf112ade431d02bb2a206597f91e2c6a26a9
Valentin Mur <valen.mur88@gmail.com>
2022-07-25 22:58:50 -0300

    agregando carritomodal y contador de productos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
+//import { stockProductos } from "./stock.js"
+
 /*===== MENU SHOW =====*/
+
+
 
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
@@ -59,60 +63,9 @@ function scrollActive() {
 ////////////////////////*===== DESAFIO ENTREGABLE CODER =====*////////////////////
 
 
-
-class Producto {
-    constructor(id, nombre, precio, stock, imagen) {
-        this.id = id
-        this.nombre = nombre
-        this.precio = precio
-        this.stock = stock
-        this.imagen = imagen
-    };
-}
-
+import { stockProductos } from "./stock.js";
 
 let carrito = []
-
-
-let stockProductos = [
-    new Producto(1, 'Nike Jordan', 149.99, 1000, "assets/img/featured1.png"),
-    new Producto(2, 'Nike Free RN', 149.99, 1000, "assets/img/featured2.png"),
-    new Producto(3, 'Nike Free RN', 149.99, 1000, "assets/img/new3.png"),
-    new Producto(4, 'Nike Sply', 79.99, 1000, "assets/img/new2.png"),
-    new Producto(5, 'Nike Sply', 79.99, 1000, "assets/img/new3.png"),
-    new Producto(6, 'Nike Sply', 79.99, 1000, "assets/img/new4.png"),
-    new Producto(7, 'Nike Sply', 79.99, 1000, "assets/img/new5.png"),
-    new Producto(8, 'Nike Sply', 129.99, 1000, "assets/img/women1.png"),
-    new Producto(9, 'Nike Sply', 129.99, 1000, "assets/img/women2.png"),
-    new Producto(10, 'Nike Sply', 129.99, 1000, "assets/img/women3.png"),
-    new Producto(11, 'Nike Sply', 129.99, 1000, "assets/img/women4.png"),
-]
-
-
-// let stockProductos = [
-//     {
-//         id: 1,
-//         nombre: 'Nike Jordan',
-//         precio: 180,
-//         stock: 1000,
-//         imagen: "assets/img/featured1.png",
-//     },
-//     {
-//         id: 2,
-//         nombre: 'Nike Free RN',
-//         precio: 180,
-//         stock: 1000,
-//         imagen: "assets/img/featured2.png",
-//     },
-//     {
-//         id: 3,
-//         nombre: 'Nike Sply',
-//         precio: 70,
-//         stock: 1000,
-//         imagen: "assets/img/new2.png",
-//     },
-
-// ]
 
 const contenedorProductos = document.getElementById('contenedor-productos')
 
@@ -138,5 +91,6 @@ const agregarAlCarrito = (idProducto) => {
 
 function carritoCompra() {
     console.log(carrito);
+
 }
 
--- assets/js/stock.js
+++ assets/js/stock.js
@@ -1,18 +1,30 @@
-let stockProductos = [
-    {
-        id: 1,
-        nombre: 'Nike Jordan',
-        precio: 180,
-        stock: 1000,
-        imagen: "assets/img/featured1.png",
-    },
-    {
-        id: 2,
-        nombre: 'Nike Sply',
-        precio: 70,
-        stock: 1000,
-        imagen: "assets/img/new2.png",
-    },
 
+
+class Producto {
+    constructor(id, nombre, precio, stock, imagen, cantidad) {
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.stock = stock
+        this.imagen = imagen
+        this.cantidad = cantidad
+    };
+}
+
+
+let stockProductos = [
+    new Producto(1, 'Nike Jordan', 149.99, 1000, "assets/img/featured1.png", 1),
+    new Producto(2, 'Nike Free RN', 149.99, 1000, "assets/img/featured2.png", 1),
+    new Producto(3, 'Nike Free RN', 149.99, 1000, "assets/img/new3.png", 1),
+    new Producto(4, 'Nike Sply', 79.99, 1000, "assets/img/new2.png", 1),
+    new Producto(5, 'Nike Sply', 79.99, 1000, "assets/img/new3.png", 1),
+    new Producto(6, 'Nike Sply', 79.99, 1000, "assets/img/new4.png", 1),
+    new Producto(7, 'Nike Sply', 79.99, 1000, "assets/img/new5.png", 1),
+    new Producto(8, 'Nike Sply', 129.99, 1000, "assets/img/women1.png", 1),
+    new Producto(9, 'Nike Sply', 129.99, 1000, "assets/img/women2.png", 1),
+    new Producto(10, 'Nike Sply', 129.99, 1000, "assets/img/women3.png", 1),
+    new Producto(11, 'Nike Sply', 129.99, 1000, "assets/img/women4.png", 1),
 ]
 
+
+export { stockProductos }
